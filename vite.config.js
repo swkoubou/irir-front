0c22a0c09d634200f31156472403c0280c8a2d52
ninjin <trisek.1003@gmail.com>
2025-06-11 07:27:54 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__direname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, ".src/components"),
-      "@css": path.resolve(__dirname, "./src/css"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@scss": path.resolve(__dirname, "./src/scss"),
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, '.src/components'),
+      '@css': path.resolve(__dirname, './src/css'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@scss': path.resolve(__dirname, './src/scss'),
     },
   },
 });
