511bab2a74f83ae0aecd458a38c49a629586d12a
kjky01 <s2421182@cco.kanagawa-it.ac.jp>
2025-06-18 18:00:54 +0900

    Revert "3 yuka"

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
-import Rain from "./components/a";
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <Rain/>
-=======
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 import App from '@pages/App';
@@ -21,6 +15,5 @@ createRoot(document.getElementById('root')).render(
         <Route path="/irir" element={<Irir />} />
       </Routes>
     </BrowserRouter>
->>>>>>> 8e5f18219f5b17fa50fd87ac2969badff8c3e922
   </StrictMode>
 );
