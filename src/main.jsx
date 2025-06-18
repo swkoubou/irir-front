7af56b006b5d37df60b39b56089b7caffb1193f5
teto-lru <mrk3hayato@gmail.com>
2025-06-18 18:04:06 +0900

    修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
-import '@css/index.css';
-import SecondResult from './components/SecondResult';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <SecondResult />
-=======
-import { BrowserRouter, Routes, Route } from 'react-router';
-
 import App from '@pages/App';
 import Rank from '@pages/rank/Rank';
 import Irir from '@pages/irir/Irir';
@@ -23,6 +13,5 @@ createRoot(document.getElementById('root')).render(
         <Route path="/irir" element={<Irir />} />
       </Routes>
     </BrowserRouter>
->>>>>>> 8e5f18219f5b17fa50fd87ac2969badff8c3e922
   </StrictMode>
 );
